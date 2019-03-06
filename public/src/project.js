760a6d476a9ef7e31606938f146e32681985745a
Fankadore <rmackenzie88@gmail.com>
2019-03-06 20:45:32 +0000

    Added play button to project previews

--- public/src/project.js
+++ public/src/project.js
@@ -1,4 +1,5 @@
 const infoIcons = document.querySelectorAll('.project__icon-info');
+const playButtons = document.querySelectorAll('.project__play');
 
 infoIcons.forEach(infoIcon => {
 	const infoId = infoIcon.id.slice(0, 5);
@@ -16,6 +17,23 @@ infoIcons.forEach(infoIcon => {
 			
 			infoBox.classList.remove('project__info--close');
 			infoBox.classList.add('project__info--open');
+		}
+	};
+});
+
+playButtons.forEach(button => {
+	const videoId = button.id.slice(0, 6);
+	const video = document.getElementById(videoId);
+	button.onclick = (e) => {
+		if (video.paused) {
+			video.play();
+			video.classList.remove('project__preview--paused');
+			button.classList.remove('project__play--paused');
+		}
+		else {
+			video.pause();
+			video.classList.add('project__preview--paused');
+			button.classList.add('project__play--paused');
 		}
 	};
 });
